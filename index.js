cce9ed23e3af8f8c5d55905fce7166ce8b9c1ea9
hbahramian <habahram@iu.edu>
2024-01-17 23:37:47 -0500

    .

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Arithmetic service - Hello World!');
 }); 
+
+app.get('/add/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) + Number(req.params.m));
+  }); 
  
 app.listen(port);
