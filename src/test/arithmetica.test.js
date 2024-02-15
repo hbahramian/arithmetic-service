445250136fb8c12a7769e3058ccd0e087f203d16
hbahramian <habahram@iu.edu>
2024-02-15 10:33:43 -0500

    .

--- src/test/arithmetica.test.js
+++ src/test/arithmetica.test.js
@@ -1,7 +1,7 @@
 const { add } = require("../arithmetica")
 
 test('2 + 3 is equal to 5', () => {
-    expect(add(2, 3)).toBe(8);
+    expect(add(2, 3)).toBe(5);
   });
 
   test('2099999999999999999 + 30000000000000 is equal to 5', () => {
