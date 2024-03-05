86ddbb4ec9f626ff6dacbb4063015c774c795c00
hbahramian <habahram@iu.edu>
2024-03-04 20:00:02 -0500

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors'); 
 const { add } = require("./arithmetica");
 const app = express();
 app.use(cors()); 
-const port = 3000; 
+
+if (!process.env.PORT) {
+  throw new Error(`Please specify the 
+  port number for the HTTP server with the environment variable PORT.`);
+}
+
+const port = process.env.PORT; 
 app.get('/', (req, res) => {
-  res.send('Arithmetic service - last updated TYPE-THE-DATE-AND-TIME-HERE');
+  res.send('Arithmetic service - last updated 3/4/2022');
 }); 
 app.get('/add/:n/:m', (req, res) => {
   let n = Number(req.params.n);
